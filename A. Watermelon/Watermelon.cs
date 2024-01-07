7ac10f1b7595e441828e6b0ac0e248e3890900e0
mahmad-alaadwy <104855133+mahmad-alaadwy@users.noreply.github.com>
2024-01-07 21:53:35 +0200

    watermelon problem to get back on the road

---
+++ A. Watermelon/Watermelon.cs
@@ -0,0 +1,20 @@
+﻿
+//var inputWatermelonWieght = int.Parse(Console.ReadLine());
+//Console.WriteLine(Watermelon.CanBeDevidedIntoTwoEvenWieghts(inputWatermelonWieght));
+
+
+namespace proplems_solved.A._Watermelon
+{
+    public static class Watermelon
+    {
+        public static string CanBeDevidedIntoTwoEvenWieghts(int wieght)
+        {
+            if ((wieght % 2) % 2 == 0)
+            {
+                return "YES";
+            }
+            return "No";
+        }
+    }
+}
+
