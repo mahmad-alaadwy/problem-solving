f7274c99ac660c53b79c84452c3e39f863e78c19
mahmad-alaadwy <104855133+mahmad-alaadwy@users.noreply.github.com>
2024-01-08 20:29:32 +0200

    A. Next Round

---
+++ A. Next Round/NextRound.cs
@@ -0,0 +1,25 @@
+﻿//using proplems_solved.A._Next_Round;
+
+//int[] nk = new int[2];
+//nk = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
+
+//int[] scores = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
+//Console.WriteLine(NextRound.GetNumberOfPassedContestants(scores, nk[1], nk[0]));
+
+
+namespace proplems_solved.A._Next_Round
+{
+    public static class NextRound
+    {
+        public static int GetNumberOfPassedContestants(int[] scores,int pivot,int length)
+        {
+            int count = 0;
+            for(int i=0;i< length; i++)
+            {
+                if (scores[i] >= scores[pivot - 1] && scores[i]>0)
+                    count++;
+            }
+            return count;
+        }
+    }
+}
