1beab1685fbe4ff6fbae86413bf18c9e8dcbdfe2
mahmad-alaadwy <104855133+mahmad-alaadwy@users.noreply.github.com>
2024-01-09 03:45:34 +0200

    Bitt++ language problem

---
+++ A. Bit++/Bit.cs
@@ -0,0 +1,26 @@
+﻿
+
+using proplems_solved.A._Bit__;
+
+int numberOfIterations = int.Parse(Console.ReadLine());
+int x = 0;
+for (int i = 0; i < numberOfIterations; i++)
+{
+    var statement = Console.ReadLine();
+    x = Bit.Execute(statement, x);
+}
+Console.WriteLine(x);
+
+namespace proplems_solved.A._Bit__
+{
+    public static class Bit
+    {
+        public static int Execute(string compiledstatement,int currentValue)
+        {
+            if (compiledstatement[1] == '+')
+                return ++currentValue;
+
+            return --currentValue;
+        }
+    }
+}
