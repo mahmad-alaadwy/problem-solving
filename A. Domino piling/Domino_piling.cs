2fed96efc8c43e959e5e0b1d6998734f33754f99
mahmad-alaadwy <104855133+mahmad-alaadwy@users.noreply.github.com>
2024-01-10 07:26:47 +0200

    A. Domino piling

---
+++ A. Domino piling/Domino_piling.cs
@@ -0,0 +1,23 @@
+﻿
+
+using proplems_solved.A._Domino_piling;
+
+int[] nk = new int[2];
+nk = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
+
+int NumberOfDominoThatFits = Domino_piling.NumberOfDominoFitInTheBoard(nk[1], nk[0]);
+Console.WriteLine(NumberOfDominoThatFits);
+
+
+namespace proplems_solved.A._Domino_piling
+{
+    public static class Domino_piling
+    {
+        public static int NumberOfDominoFitInTheBoard(int l, int t)
+        {
+            //each domino fits 2 meters then the number of domino = the area//2;
+            int area = l * t;
+            return (int)area / 2;
+        }
+    }
+}
