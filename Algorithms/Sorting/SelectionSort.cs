c81ae5984c1757e47ec78ce5c1efbcf7787b1f7a
mahmad-alaadwy <104855133+mahmad-alaadwy@users.noreply.github.com>
2024-01-12 11:58:55 +0200

    Selection Sort Algorithm

---
+++ Algorithms/Sorting/SelectionSort.cs
@@ -0,0 +1,31 @@
+﻿namespace proplems_solved.Algorithms.Sorting
+{
+    public static class SelectionSort<T> where T : IComparable
+    {
+        public static T[] Sort(T[] inputArray)
+        {
+            int length = inputArray.Length;
+            int minimumIndx;
+
+            for (int j = 0; j < length; j++)
+            {
+                minimumIndx = j;
+                for (int i = j; i < length; i++)
+                {
+                    if (inputArray[i].CompareTo(inputArray[minimumIndx]) <= 0)
+                        minimumIndx = i;
+                }
+                if (minimumIndx != j)
+                    SwapElements(minimumIndx, j, inputArray);
+            }
+            return inputArray;
+        }
+
+        private static void SwapElements(int index1, int index2, T[] containingArray)
+        {
+            T temp = containingArray[index1];
+            containingArray[index1] = containingArray[index2];
+            containingArray[index2] = temp;
+        }
+    }
+}
